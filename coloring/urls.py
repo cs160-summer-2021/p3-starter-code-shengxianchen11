c274838cb5163ecf150320fa639c3c8ff18fe0a7
Shengxian Chen <connnorchen@berkeley.edu>
2021-07-13 16:24:55 -0700

    no background feature

--- coloring/urls.py
+++ coloring/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction')
+    path('new_interaction', views.ni, name='new_interaction'),
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+
+def ni(request):
+    return render(request, 'coloring/new_interaction.html')
