3e794c1844855b8ddd01c7b1bcb92cc942407428
Shengxian Chen <connnorchen@berkeley.edu>
2021-07-14 12:09:28 -0700

    pj3 done

--- coloring/urls.py
+++ coloring/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('demo', views.index, name='demo'),
-    path('new_interaction', views.ni, name='new_interaction'),
+    path('new_interaction', views.interaction, name='new_interaction'),
+    path('triangle_coloring', views.triangle, name='triangle_coloring'),
+    path('home_page', views.home, name='home_page'),
+    path('cover', views.cover, name='cover')
 ]
+
--- coloring/views.py
+++ coloring/views.py
@@ -3,5 +3,14 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'coloring/index.html')
 
-def ni(request):
+def interaction(request):
     return render(request, 'coloring/new_interaction.html')
+
+def triangle(request):
+    return render(request, 'coloring/triangle_coloring.html')
+
+def home(request):
+    return render(request, 'coloring/home_page.html')
+
+def cover(request):
+    return render(request, 'coloring/cover.html')
